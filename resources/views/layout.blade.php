df7c1366fb92e176932e20dca3ddd6e30913aaa8
marcella <marcellavanduyn@gmail.com>
2019-01-28 16:02:19 +0100

    layout blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,18 +9,20 @@
     <title>@yield('title')</title>
 </head>
 <style>
-
+body{
+margin:0;
+padding:0;
+}
 .logo{
     height: 100px;
     width: 100px;
-    }
-    #text{
-        margin:40px;
-    }            
+    } 
+           
     .social:hover {
             -webkit-transform: scale(1.1);
             -moz-transform: scale(1.1);
             -o-transform: scale(1.1);
+            color:#99C4CC;
         }
         
             .social {
@@ -31,6 +33,13 @@
             -webkit-transition-duration: 0.5s;
             -moz-transition-duration: 0.5s;
             -o-transition-duration: 0.5s;
+            color:#27646E;
+
+        }
+        .txt{
+            font-size: 23px; 
+            font-weight: bold; 
+            color:#27646E;
         }
         #blog{
             width:80%;
@@ -39,18 +48,58 @@
             margin-right: auto;
 
         }
+
+        .icon-bar {
+            position: fixed;
+            top: 50%;
+            -webkit-transform: translateY(-50%);
+            -ms-transform: translateY(-50%);
+            transform: translateY(-50%);
+         }
+         .icon-bar a {
+             display: block;
+             text-align: center;
+             padding: 16px;
+             transition: all 0.3s ease;
+             color: white;
+            font-size: 20px;
+        }
+        .icon-bar a:hover {
+  background-color: #99C4CC;
+}
+.linkedin {
+  background: #007bb5;
+  color: white;
+}
+.github{
+    background-color: #000;
+    color:white;
+}
+.mail{
+    background-color: red;
+    color: white;
+}
+.web{
+    background-color: #27646E;
+    color: white;
+}
         
 </style>
 <body>
- <nav class="nav justify-content-center">
-    <a class="navbar-brand" href="/"><a href="/"><img class="logo" src="image/logo.PNG" alt="logo" ></a>
-   <a class="nav-link active" href="/bootcamp">Bootcamp</a>
-   <a class="nav-link"  href="#">Hackaton</a>
-   <a class="nav-link"  href="#">Blog</a>
-   <a class="nav-link" href="#">Skills</a>
-   <a class="nav-link" href="https://www.linkedin.com/in/marcella-hadderingh-013115176/"><i class="fab fa-linkedin fa-2x social"></i> </a>
-   <a  class="nav-link" href="https://github.com/MarcellaHadderingh"><i class="fab fa-github-square fa-2x social"></i></a>
+ <nav class="nav justify-content-center ">
+    <a class="navbar-brand social" href="/"><a href="/"><img class="logo social" src="image/logo.PNG" alt="logo" ></a>
+   <a class="nav-link social txt" href="/bootcamp">Bootcamp</a>
+   <a class="nav-link social txt "  href="#">Hackaton</a>
+   <a class="nav-link social txt "  href="/blog">Blog</a>
+   <a class="nav-link social txt" href="#">Skills</a>
 </nav>
+
+<div class="icon-bar">
+        <a href="https://www.linkedin.com/in/marcella-hadderingh-013115176/" class="linkedin"><i class="fab fa-linkedin fa-1x"></i></a>
+        <a href="https://github.com/MarcellaHadderingh" class="github"><i class="fab fa-github fa-1x"></i></a> 
+        <a href="mailto:marcellavanduyn@gmail.com" class="mail"><i class="fa fa-envelope fa-1x"></i></a> 
+        <a href="http://marcellahadderingh.herokuapp.com" class="web"><i class="fab fa-medium-m fa-1x"></i></a>
+    </div>
 
     @yield('content')
     
