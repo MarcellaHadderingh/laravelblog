3aa02355d64c43644fd56da140f70d69dd88cd24
marcella <marcellavanduyn@gmail.com>
2019-01-28 16:01:07 +0100

    text added

--- resources/views/bootcamp.blade.php
+++ resources/views/bootcamp.blade.php
@@ -5,14 +5,19 @@
 @section('content')
 
     <img src="image/bootcamp.PNG" class="img-fluid" alt="Responsive image">
-    <div id="text" class="container-fluid">
-        <h1> Bootcamp Code Gorilla Enschede 2018/2019</h1>
-        <BR>
-        <p>CodeGorilla, een programmeerschool waar werkzoekenden worden omgeschoold tot junior developer (programmeur), zet voet aan de grond in Twente.<BR> Via CodeGorilla kunnen werkzoekenden als onder andere webdeveloper aan de slag in het bedrijfsleven.</p>
-        <p>Je hebt geen kennis van programmeren nodig, moet wel Nederlands en Engels kunnen maar vooral een doorzetter zijn.
-            </p>
-
-              <p>  Er is landelijk dringend behoefte aan goed opgeleid ICT-personeel. Voor de aankomende bootcamp, een korte maar intensieve cursus.</p>
-          </div>
+    
+    
+    <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+              <h1 class="display-4">Bootcamp Code Gorilla Enschede 2018/2019</h1>
+              <p class="lead"> CodeGorilla, een programmeerschool waar werkzoekenden worden omgeschoold tot junior developer (programmeur).<BR> Via CodeGorilla kunnen werkzoekenden als onder andere webdeveloper aan de slag in het bedrijfsleven.</p>
+        <p>Je hebt geen kennis van programmeren nodig, moet wel Nederlands en Engels kunnen maar vooral een doorzetter zijn.</p>
+        <p>Er is landelijk dringend behoefte aan goed opgeleid ICT-personeel. De bootcamp is een korte maar intensieve cursus.</p>
+        <p>De unieke visie van CodeGorilla werpt inmiddels zijn vruchten af. De programmeerschool, die sinds begin 2017 bestaat, boekt flink succes: 43 werkzoekenden hebben zich al succesvol omgeschoold tot junior developer en een baan gevonden. Het bedrijf heeft meerdere vestigingen in Nederland en gaat nu een ICT-bootcamp organiseren in Enschede. Via deze bootcamp worden studenten in een korte, intensieve periode opgeleid tot junior developers. Deelnemers leren de fijne kneepjes van het development-vak door te werken met opdrachten uit het bedrijfsleven. In 2017 won CodeGorilla de Social Impact Award, een prijs voor een jong bedrijf dat niet alleen economisch, maar ook maatschappelijk een rol van betekenis speelt.</p>
+        <p> Deze cursus biedt mensen die een uitkering hebben de kans om zich in vier maanden tijd om te scholen tot programmeur, zodat zij een betere positie krijgen op de arbeidsmarkt. Dankzij de gemeente kunnen de deelnemers de bootcamp geheel gratis volgen.</p>
+        <p>Tijdens de bootcamp leren de deelnemers volgens het opleidingsinstituut ‘alle vaardigheden die nodig zijn voor een baan in de it, en is er aandacht voor het leren van soft skills zoals communiceren en samenwerken. Daarnaast worden de deelnemers geholpen bij het vinden van een geschikte baan'. Hiermee probeert CodeGorilla naar eigen zeggen twee problemen op te lossen: enerzijds de werkloosheid, en anderzijds de toenemende vraag naar programmeurs op de arbeidsmarkt. </p>
+        <p><strong>Wat leren de deelnemers in de cursus? Welke programmeertalen moeten ze onder de knie krijgen?</strong><br>
+            ‘Ze leren PhP, html, CSS en JavaScript. De opleiding bestaat uit één maand zelfstudie, die we tevens gebruiken als selectiemiddel. Na deze maand worden de cursisten uitgedaagd in een hackathon, waarbij ze tijdens een weekend verschillende opdrachten moeten uitvoeren. Hierbij willen we de deelnemers beter leren kennen en vooral hun soft skills meten. Ze moeten in groepen samenwerken en worden ook onder (tijds)druk gezet. Tijdens deze hackathon krijgen ze casussen uit het bedrijfsleven voorgeschoteld.<br>Na deze periode volgt er een bootcamptraject van drie maanden.   Deze opleiding is zeer intensief en bedraagt zeker veertig uur per week.</p>
+    </div>
    
 @endsection
