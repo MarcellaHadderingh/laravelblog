93db5d04bc29cd1143bcb349afef86ca969f7e8a
marcellahadderingh <marcellavanduyn@gmail.com>
2019-01-31 13:14:01 +0100

    added some more text

--- resources/views/bootcamp.blade.php
+++ resources/views/bootcamp.blade.php
@@ -10,7 +10,7 @@
     <div class="jumbotron jumbotron-fluid">
             <div class="container">
               <h1 class="display-4">Bootcamp Code Gorilla Enschede 2018/2019</h1>
-              <p class="lead"> CodeGorilla, een programmeerschool waar werkzoekenden worden omgeschoold tot junior developer (programmeur).<BR> Via CodeGorilla kunnen werkzoekenden als onder andere webdeveloper aan de slag in het bedrijfsleven.</p>
+              <p class="lead"> CodeGorilla, een programmeerschool waar werkzoekenden worden omgeschoold tot junior developer (programmeur).</p><p>Via CodeGorilla kunnen werkzoekenden als onder andere webdeveloper aan de slag in het bedrijfsleven.</p>
         <p>Je hebt geen kennis van programmeren nodig, moet wel Nederlands en Engels kunnen maar vooral een doorzetter zijn.</p>
         <p>Er is landelijk dringend behoefte aan goed opgeleid ICT-personeel. De bootcamp is een korte maar intensieve cursus.</p>
         <p>De unieke visie van CodeGorilla werpt inmiddels zijn vruchten af. De programmeerschool, die sinds begin 2017 bestaat, boekt flink succes: 43 werkzoekenden hebben zich al succesvol omgeschoold tot junior developer en een baan gevonden. Het bedrijf heeft meerdere vestigingen in Nederland en gaat nu een ICT-bootcamp organiseren in Enschede. Via deze bootcamp worden studenten in een korte, intensieve periode opgeleid tot junior developers. Deelnemers leren de fijne kneepjes van het development-vak door te werken met opdrachten uit het bedrijfsleven. In 2017 won CodeGorilla de Social Impact Award, een prijs voor een jong bedrijf dat niet alleen economisch, maar ook maatschappelijk een rol van betekenis speelt.</p>
@@ -18,6 +18,32 @@
         <p>Tijdens de bootcamp leren de deelnemers volgens het opleidingsinstituut ‘alle vaardigheden die nodig zijn voor een baan in de it, en is er aandacht voor het leren van soft skills zoals communiceren en samenwerken. Daarnaast worden de deelnemers geholpen bij het vinden van een geschikte baan'. Hiermee probeert CodeGorilla naar eigen zeggen twee problemen op te lossen: enerzijds de werkloosheid, en anderzijds de toenemende vraag naar programmeurs op de arbeidsmarkt. </p>
         <p><strong>Wat leren de deelnemers in de cursus? Welke programmeertalen moeten ze onder de knie krijgen?</strong><br>
             ‘Ze leren PhP, html, CSS en JavaScript. De opleiding bestaat uit één maand zelfstudie, die we tevens gebruiken als selectiemiddel. Na deze maand worden de cursisten uitgedaagd in een hackathon, waarbij ze tijdens een weekend verschillende opdrachten moeten uitvoeren. Hierbij willen we de deelnemers beter leren kennen en vooral hun soft skills meten. Ze moeten in groepen samenwerken en worden ook onder (tijds)druk gezet. Tijdens deze hackathon krijgen ze casussen uit het bedrijfsleven voorgeschoteld.<br>Na deze periode volgt er een bootcamptraject van drie maanden.   Deze opleiding is zeer intensief en bedraagt zeker veertig uur per week.</p>
-    </div>
+            <p>Na onze bootcamp is het tijd om de de wijde IT-wereld in te gaan. Daarom ontwikkel je als CodeGorilla trainee een breed scala aan vaardigheden. De vraag op de arbeidsmarkt bepaalt op welke vaardigheden de nadruk wordt gelegd. Een CodeGorilla trainee beheerst ten minste de onderstaande onderwerpen. Er is ruimte voor extra verdieping, mits je de basis beheerst.</p>
+            <ul>
+                <li><strong><i class="far fa-eye"></i> FRONT-END DEVELOPMENT</strong></li>
+                 HTML, CSS, Bootstrap, JavaScript, Angular
+                <li><strong><i class="far fa-keyboard"></i> GENERAL DEVELOPMENT</strong></li>
+                 Scrum with Trello, Visual Studio code, GIT version control, Cloud deployment, OWASP websecurity
+                <li><strong> <i class="fas fa-laptop"></i> BACK-END DEVELOPMENT</strong></li>
+                 PHP, Laravel, MySQL, Linux
+                <li><strong><i class="far fa-comment"></i> SOFT SKILLS</strong></li>
+                Self improvement, Communication, Problem solving, Time management
+            </ul>
+        </div>
+
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+              <h1 class="display-4">CodeGorilla Bootcamp</h1>
+              <p><strong><i class="fas fa-book"></i>(1) Thuisstudie</strong></p>
+              <p>Maak kennis met het programmeer vak en sleep je toegangskaartje voor de Hackathon binnen.</p>
+              <p><strong><i class="fas fa-cubes"></i>(2) Hackathon</strong></p>
+              <p>Pas je geleerde vaardigheden uit de thuisstudie toe in de praktijk.<br> Los een casus uit het bedrijfsleven op en laat de Gorilla in jezelf zien!</p>
+              <p><strong><i class="fas fa-code"></i>(3) Bootcamp</strong></p>
+              <p>Heb jij de selectieprocedure doorstaan? Dan gaat het echte werk nu beginnen. <br>Je leert verschillende programmeertalen en je wordt klaargestoomd voor een baan als developer.</p>
+              <p><strong><i class="fas fa-trophy"></i>(4) Work</strong></p>
+              <p>Samen gaan we opzoek naar een baan in de IT. <br>Je bouwt aan projecten en krijgt de kans om dit gelijk te laten zien aan interessante werkgevers.</p>
+            </div>
+        </div>
+
    
 @endsection
