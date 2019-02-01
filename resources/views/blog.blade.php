896ab543a32fa19d589a4ae8d81d7e1cc9ebad1b
marcellahadderingh <marcellavanduyn@gmail.com>
2019-02-01 09:11:22 +0100

    chunck posts

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -8,8 +8,10 @@
 <img src="image/blog.PNG" class="img-fluid" alt="Responsive image">
 
 
-@foreach($posts as $post)
-<div class="card bg-light mb-3 card" style="max-width: 18rem; margin:40px 0 0 60px; float:left;">
+@foreach($posts->chunk(3) as $postChunk)
+<div class="row">
+@foreach($postChunk as $post)
+<div class="card bg-light mb-3 card" style="max-width: 18rem; margin:50px 0 0 100px; float:left;">
         <div class="card-header"><h5>{{ $post->title }}</h5></div>
         <div class="card-body">
           <p class="card-title" style="text-align:center; font-size:12px; font-style:italic;">{{ $post->updated_at}}</p>
@@ -17,5 +19,12 @@
         </div>
       </div>
       @endforeach
+    </div>
+    @endforeach
+
     
+
+
+
 @endsection
+
